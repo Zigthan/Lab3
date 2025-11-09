65d71e40b489da91b53d16fc6ba144d775bcf3bc
Ethan Daniel <EthanBaliwagDB42.24@ichat.sp.edu.sg>
2025-11-09 23:53:18 +0800

    bmi testing

--- test_bmi.py
+++ test_bmi.py
@@ -1,5 +1,6 @@
 import Lab2.bmi as bmi
 
+# Follow the same nameing convention
 def test_bmi_normal_weight():
     result = bmi.calculate_bmi(1.75, 70)
     assert result == 0
@@ -11,3 +12,21 @@ def test_bmi_over_weight():
 def test_bmi_under_weight():
     result = bmi.calculate_bmi(1.75, 50)
     assert result == -1
+
+
+# The AAA pattern in Testing
+
+# The Three Phases
+
+# 1. Arrange 
+# Set up the test conditions
+# Prepare the input data and prerequisites
+
+# 2. Act
+# Call the function/method you want to test
+# This is usually a single line
+
+# 3. Assert
+# Verify the results
+# Check the actual output matches the expected output
+# Use assertions to confirm correctness.
